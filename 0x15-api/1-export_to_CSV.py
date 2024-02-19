c55908043160d9fdd291ac8e40a9f2ad5c34bb75
markrizkalla <mark.rizkalla1@gmail.com>
2024-02-20 01:02:37 +0200

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format."""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    id = int(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get(url + '/users').json()
+    user_name = users[id - 1].get('username')
+
+    tasks = requests.get(url + '/todos').json()
+
+    with open(f"{str(id)}.csv", 'w') as f:
+        spamwriter = csv.writer(f, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            if task.get('userId') == id:
+                spamwriter.writerow([str(id), user_name,
+                                    str(task.get('completed')),
+                                    task.get('title')])
